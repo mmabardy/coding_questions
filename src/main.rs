c8a818da78c99f477a18515e7550fec629654f76
mmabardy <firstru7h@gmail.com>
2020-03-05 17:31:31 -0500

    Add some more coding questions I've had

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,9 @@ fn main() {
     I carry them, men and women, I carry them with me wherever I go,
     I swear it is impossible for me to get rid of them,
     I am fill’d with them, and I will fill them in return.");
+    
+    let to_compress = String::from("Thissss stringgggg toooo beee compressedddd");
+    let compressed = String::from("This3 string4 to3 be2 compres1ed3");
     let temp = word_count(&address_string);
     //top_k_frequent(address_string, 10);
     let mut count_vec: Vec<_> = temp.iter().collect();
@@ -26,9 +29,58 @@ fn main() {
     let mut temp3 = address_string.split(|c: char| !c.is_alphanumeric())
     .filter(|w| !w.is_empty())
     .map(|w| w.to_lowercase())
-    .fold(HashMap::new(), countWords);
+    .fold(HashMap::new(), count_words);
     println!("temp3: {:?}", temp3);
+
     
+    let compress_test = compress(&to_compress);
+    println!("{}", compress_test);
+    let test_parens = String::from("()");
+    println!("{}", balanced_parens(test_parens));
+}
+
+fn create_part(character: char, count: i32) -> String {
+    let count = if count == 1 { 
+        "".to_string() 
+    } else {
+        count.to_string() 
+    };
+    println!("Char: {}, Count: {}", character, count);
+    format!("{}{}", character, count)
+}
+
+fn compress(text: &str) -> String {
+    // If input is null string, just return that
+    if text == "" {
+        return text.to_string()
+    }
+
+    // Converts input string into vector of characters
+    let chars: Vec<char> = text.chars().collect();
+    // Create and set some temp variables
+    let mut result = String::new();
+    let mut prev_char = chars[0];
+    let mut count = 0;
+    // Loop through vector of chars, if the current char is equal to previous char, 
+    // increment count, else: push the result of create_part onto the string result
+    for c in 0..(chars.len() - 1) {
+        if chars[c] == prev_char {
+            count += 1;
+        } else {
+            result.push_str(create_part(prev_char, count).as_str());
+            prev_char = chars[c];
+            count = 1;
+        }
+    }
+
+    // Needs to be called again because chars is not empty
+    result.push_str(create_part(prev_char, count).as_str());
+    // If text and result are the same length, return the text variable as a string
+    if text.len() == result.len() {
+        String::from(text)
+    } else {
+        result
+    }
 }
 
 fn top_k_frequent(input_string: String, k: usize){
@@ -51,14 +103,73 @@ fn word_count(sentence: &str) -> HashMap<String, u32> {
     sentence.split(|c: char| !c.is_alphanumeric())
         .filter(|w| !w.is_empty())
         .map(|w| w.to_lowercase())
-        .fold(HashMap::new(), countWords)
+        .fold(HashMap::new(), count_words)
 }
 
-fn countWords(mut hashMap: HashMap<String, u32>, word: String) -> HashMap<String, u32> {
+fn count_words(mut hashMap: HashMap<String, u32>, word: String) -> HashMap<String, u32> {
     {
         let c = hashMap.entry(word).or_insert(0);
         *c += 1;
     }
 
     hashMap
+}
+
+// does no error checking, but takes a string of parens and checks if it's balanced
+fn balanced_parens(input: String) -> bool {
+    let mut output = false;
+    let mut left_count = 0;
+    let mut right_count = 0;
+    for i in 0..input.len() {
+        if input.chars().nth(i).unwrap() == '('{
+            left_count += 1;
+        } else {
+            right_count += 1;
+        }
+    }
+    if left_count == right_count {
+        return true;
+    } else {
+        return false;
+    }
+    
+}
+
+//This is someone else's code: jesskay on exercism and github
+//TO DO: rewrite my own way
+pub struct Brackets { is_balanced: bool }
+
+impl Brackets {
+    pub fn from(s: &str) -> Self {
+        let mut open_brackets_stack = Vec::new();
+
+        for c in s.chars() {
+            match c {
+                // opening a bracket/brace is always fine
+                '{' | '(' | '[' => open_brackets_stack.push(c),
+                // closing is only fine when it matches the most recent opened
+                '}' | ')' | ']' => {
+                    if let Some(prev) = open_brackets_stack.pop() {
+                        match (prev, c) {
+                            // closing correct pair, all fine (already popped)
+                            ('{', '}') | ('(', ')') | ('[', ']') => continue,
+                            // closing a wrong pair, bail
+                            _ => return Self { is_balanced: false }
+                        }
+                    } else { // closing without any open pair, bail
+                        return Self { is_balanced: false };
+                    }
+                },
+                // ignore anything that's not a bracket, for this exercise
+                _ => continue
+            }
+        }
+
+        // only balanced if we didn't bail early *and* nothing left on the stack
+        Self { is_balanced: open_brackets_stack.len() == 0 }
+    }
+
+    pub fn are_balanced(&self) -> bool {
+        self.is_balanced
+    }
 }
